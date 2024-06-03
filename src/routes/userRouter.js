33af0538a5951d0d1e610cf1e05c0579f648343e
caro3221 <86697084+caro3221@users.noreply.github.com>
2024-06-03 00:56:18 -0300

    Se agregan rutas para adm

--- src/routes/userRouter.js
+++ src/routes/userRouter.js
@@ -20,4 +20,10 @@ userRouter.patch('/api/users/:id', updateUser);
 
 userRouter.delete('/api/users/:id', deleteUser);
 
+// Rutas accesibles solo por administradores
+userRouter.post('/api/admin/createUser', validateToken, checkRole('ADMIN'), createUser);
+userRouter.delete('/api/admin/users/:id', validateToken, checkRole('ADMIN'), deleteUser);
+
+module.exports = userRouter;
+
 module.exports = userRouter;
