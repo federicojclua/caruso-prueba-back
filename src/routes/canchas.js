cf043e4d6a4a7414afe335043c46cf50af2d4bd3
Lucho <cheluiviajes@gmail.com>
2024-06-05 22:24:44 -0300

    limpieza canchas.js

--- src/routes/canchas.js
+++ src/routes/canchas.js
@@ -1,7 +1,7 @@
 // src/routes/canchas.js
 const express = require('express');
 const router = express.Router();
-const { obtenerCanchasDisponibles, reservarCancha, reservarTodasCanchas } = require('../controllers/canchasController');
+const { obtenerCanchasDisponibles } = require('../controllers/canchasController');
 
 router.get('/:sucursal/canchas-disponibles', obtenerCanchasDisponibles);
 
