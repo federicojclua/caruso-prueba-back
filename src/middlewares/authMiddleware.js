a0c7cd2ad4cfdfcb9e85f840667ee5498eea9773
caro3221 <86697084+caro3221@users.noreply.github.com>
2024-06-11 15:19:52 -0300

    se modifica authMiddleware.js

--- src/middlewares/authMiddleware.js
+++ src/middlewares/authMiddleware.js
@@ -1,36 +1,25 @@
-const jwt = require("jsonwebtoken");
-const User = require("../models/userModel"); // Asegúrate de que la ruta al modelo de usuario es correcta
+// src/middlewares/authMiddleware.js
+const jwt = require('jsonwebtoken');
+const User = require('../models/userModel');
 
 async function authMiddleware(req, res, next) {
-	const token = req.header("Authorization")?.replace("Bearer ", "");
-	if (!token) {
-		return res
-			.status(401)
-			.json({ message: "No se proporcionó un token de autenticación" });
-	}
-	try {
-		const decoded = jwt.verify(token, process.env.JWT_SECRET);
-		const user = await User.findById(decoded.id);
-		if (!user) {
-			return res.status(401).json({ message: "Usuario no encontrado" });
-		}
-		req.user = user;
-		next();
-	} catch (error) {
-		res.status(401).json({ message: "Token no válido" });
-	}
+  const token = req.header('Authorization')?.replace('Bearer ', '');
+  if (!token) {
+    return res.status(401).json({ message: 'No se proporcionó un token de autenticación' });
+  }
+
+  try {
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+    const user = await User.findById(decoded.id);
+    if (!user) {
+      return res.status(401).json({ message: 'Usuario no encontrado' });
+    }
+    req.user = user;
+    next();
+  } catch (error) {
+    res.status(401).json({ message: 'Token no válido' });
+  }
 }
+
 module.exports = authMiddleware;
 
-function checkRole(role) {
-	return (req, res, next) => {
-		if (req.user && req.user.role === role) {
-			next();
-		} else {
-			res
-				.status(403)
-				.json({ message: "Acceso denegado: permisos insuficientes" });
-		}
-	};
-}
-module.exports = checkRole;
