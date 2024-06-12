ecbb46f1cfc9541bf8c54df8242c7af011c2e3b3
Lucho <cheluiviajes@gmail.com>
2024-06-11 21:55:56 -0300

    validateProduct modificado para renderizar en front

--- src/middlewares/validateProduct.js
+++ src/middlewares/validateProduct.js
@@ -1,6 +1,6 @@
 module.exports = (req, res, next) => {
-    const { cantidad } = req.body;
-    if (cantidad < 0) {
+    const { quantity } = req.body;
+    if (quantity < 0) {
         return res.status(400).send({ error: 'La cantidad del producto no puede ser menor a 0.' });
     }
     next();
